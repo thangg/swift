8d8dbf2bf353d835a54c0d18a164c481c790d14d
ECO0813-THANGNV <thangnguyen4@tdt.asia>
2022-10-26 10:39:03 +0700

    First commit

---
+++ DemoSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoSwift
+//
+//  Created by ECO0813-THANGNV on 10/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
